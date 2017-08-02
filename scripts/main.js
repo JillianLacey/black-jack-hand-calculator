d8698132af0d27d1354f501b1375ac7195bdbbcf
Jillian Pichocki <jpichocki@gmail.com>
2017-08-02 15:08:00 -0500

    Completed Assignment

--- scripts/main.js
+++ scripts/main.js
@@ -10,17 +10,41 @@
 */
 
 
-function handValue (hand) {
+function handValue(hand) {
+  let total = 0;
+
+  for (var i = 0; i < hand.length; i++) {
 
 
-  return;
+    if ((hand[i] === "J") || (hand[i] === "Q") || (hand[i] === "K")) {
+      hand[i] = "10";
+      total += parseInt(hand[i]);
+    } else if (hand[i] !== "A") {
+      total += parseInt(hand[i]);
+    }
+
+  }
+
+  for (var k = 0; k < hand.length; k++) {
+    if (hand[k] === "A") {
+      if (total >= 11) {
+        hand[k] = "1";
+        total += parseInt(hand[k]);
+      } else {
+        hand[k] = "11";
+        total += parseInt(hand[k]);
+      }
+    }
+
+  }
+  console.log(hand, total);
+  return total;
 }
 
 
-/* -----  Hints ------
 
-1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
-K, Q, J ==> Worth 10
-A       ==> Worth 1 or 11
 
-*/
+
+// 1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
+// K, Q, J ==> Worth 10
+// A       ==> Worth 1 or 11
